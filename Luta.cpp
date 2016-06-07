d09bcb95591f435c3788f5152cd0e352bd36ca64
Mychellangello <mychellangello@hotmail.com>
2016-06-07 19:24:42 -0300

    Create Luta.cpp

---
+++ Luta.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <string>//Para poder usar o tipo de dado String.
+#include <ctime>//Para poder usar o tempo como "semente" do gerador.
+
+using namespace std;
+int main(int argc, char* args[])
+{
+   //Variavel para sair ou permanecer no laço
+	bool sair = false;
+  //Variavel que guarda os nomes dos jogadores
+	string jog1, jog2; 
+  //valor da vida inicial 
+	int vida1 = 20, vida2 = 20; 
+  //valor do ataque e defesa
+	int A = 4, D = 2; 
+
+  // imprimi na tela 
+	cout << "Digite o nome do jogador 1:" << endl; 
+  //Salva o nome digitado na variavel jog1
+	cin >> jog1; 
+
+  // imprimi na tela 
+	cout << "Digite o nome do jogador 2:" << endl; 
+	//Salva o nome digitado na variavel jog2
+	cin >> jog2;
+	//enquanto sair é falso ele repeti o laço
+	while (sair == false) {
+
+		//Imprimindo os personagens e os status
+		cout << " o                       o" << endl;
+		cout << ".T./                   \\.T." << endl;
+		cout << " ^                       ^" << endl;
+		cout << jog1 << "             " << jog2 << endl;
+		cout << "Vida:" << vida1 << "           " << "Vida:" << vida2 << endl;
+		cout << "A:" << A << "           " << "A:" << A << endl;
+		cout << "D:" << D << "           " << "D:" << D << endl;
+
+  
+    //faz com que, as "coisas" imprimidas na tela continue na tela
+		system("pause"); 
+
+		int dado6;
+
+		//gerador de numeros aleatorio em função do tempo
+		srand((int)time(0));
+		//Gera um número aletorio
+		dado6 = rand() % 6 + 1;
+
+		//se a condição do if for verdadeira ele roda, oq esta dentro do if 
+		if (dado6 > 4)
+		{
+		  // vida menos o dano gerado
+			vida1 = vida1 - dado6; 
+		  // Mostra o dano causado/ a vida perdida 
+			cout << jog1 << " Dano:" << -dado6 << endl; 
+			// imprimi na tela 
+			cout << jog2 << " Acertou!" << endl;
+			cout << " Proxima jogada (Digite enter):" << endl;
+			//"pausa" o jogo até aperta enter para ir à proxima partida 
+			cin.get();
+		}
+		//Se for verdadeiro a condção ele executa o que esta dentro do else if 
+		else if (dado6 <= 4)
+		{
+
+			//imprimi na tela 
+			cout << jog2 << "Errou!" << endl; /
+			cout << "Proxima jogada (Digite enter):" << endl
+			cin.get();
+
+		}
+			//Gera um número aletorio
+		dado6 = rand() % 6 + 1;
+
+		
+		if (dado6 > 4)
+		{
+
+			vida2 = vida2 - dado6; 
+			cout << jog2 << "Dano:" << -dado6 << endl;
+			cout << jog1 << "Acertou!" << endl;
+			cout << "Proxima jogada (Digite enter):" << endl; 
+			cin.get();
+		}
+
+		
+		else if (dado6 <= 4) {
+
+			cout << jog1 << "Errou!" << endl; 
+			cout << "Proxima jogada (Digite enter):" << endl; 
+			cin.get();
+
+		}
+
+		//cout << "Vida:" << vida1 << "           " << "Vida:" << vida2 << endl;
+   
+    //Limpa a tela ao final de cada rodada 
+		system("cls"); 
+
+		//se a vida, tanto do jog1 ou jog2 chega a zero, sair é verdadeiro, não limpara a tela 
+		if (vida1 <= 0 || vida2 <= 0)
+		{
+
+			sair = true;
+		}
+		// se continuar limpa a tela
+		else system("cls");
+	}
+
+	//imprimi na tela
+	cout << "Fim de jogo!" << endl;
+
+	//faz a logica do "se" e imprimi na tela determindo o vencedor 
+	if (vida1 > 0) cout << jog1 << "Venceu!" << endl;
+	else if (vida2 > 0) cout << jog2 << "Venceu!" << endl;
+	else cout << "Os dois perderam!" << endl;
+	return 0;
+
+
+}
